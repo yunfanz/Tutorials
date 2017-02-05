629f7aaf8ecadcbce3d50dc3c47b77f013587fef
Yunfan Zhang <yunfanz@berkeley.edu>
2017-02-04 23:03:15 -0800

    sync

---
+++ cpp/HelloWorld.cpp
@@ -0,0 +1,9 @@
+/*commentsth more comments */
+
+#include <iostream>
+
+int main ()
+{
+  std::cout << "Hello World! \n";     // prints Hello World!
+  std::cout << "I'm a C++ program\n"; // prints I'm a C++ program
+}
